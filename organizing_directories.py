d87b7de1d42798c6f97cb088c3785e4c0d7e729d
ibrahimrehman88 <ibrahimrehman88@gmail.com>
2023-03-03 17:44:58 +0500

    comment print lines

--- organizing_directories.py
+++ organizing_directories.py
@@ -21,7 +21,7 @@ directorytoorganize = Path(getdirectory)
 
 for item in directorytoorganize.iterdir():
     filepath = Path(item)
-    print(filepath)
+    #print(filepath)
     filename = os.path.basename(filepath)
 
     filetype = filepath.suffix.lower()
@@ -31,8 +31,8 @@ for item in directorytoorganize.iterdir():
     # directory = pickdirectory(filetype)
     # directorypath = Path(directory)
 
-    print(directory)
-    print("####directory path" , directoryPath)
+    #print(directory)
+    #print("####directory path" , directoryPath)
 
     newdirectory = os.path.join(directorytoorganize, directory)
     newdirectorypath = Path(newdirectory)
@@ -46,33 +46,3 @@ gc = pickdirectory('.jpg')
 print(gc)
 
 
-
-# import os
-# from pathlib import Path
-# SUBDIRECTORIES = {
-#     "DOCUMENTS": ['.pdf','.rtf','.txt'],
-#     "AUDIO":['.m4a','.m4b','.mp3'],
-#     "VIDEOS": ['.mov','.avi','.mp4'],
-#     "IMAGES": ['.jpg','.jpeg','.png']
-# }
-# def pickDirectory(value):
-#     for category, suffixes in SUBDIRECTORIES.items():
-#         for suffix in suffixes:
-#             if suffix == value:
-#                 return category
-#     return 'MISC' #If filetype doesn't exist in our dictionary
-
-# def organizeDirectory():
-#     for item in os.scandir():
-#         if item.is_dir():
-#             continue
-#         filePath = Path(item)
-#         print(filePath)
-#         filetype = filePath.suffix.lower()
-#         directory = pickDirectory(filetype)
-#         directoryPath = Path(directory)
-#         if directoryPath.is_dir() != True:
-#             directoryPath.mkdir()
-#         filePath.rename(directoryPath.joinpath(filePath))
-
-# organizeDirectory()
